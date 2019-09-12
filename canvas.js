de9fa06d1c5149649d360449d4761c0ada772289
Karlo Škafec <karlo.skafec@gmail.com>
2019-09-12 09:52:44 +0200

    Starting template from Dev Ed tutorial on Canvas

---
+++ canvas.js
@@ -0,0 +1,55 @@
+drawing();
+
+function drawing() {
+    window.addEventListener("load", () => {
+        const canvas = document.querySelector("#canvas");
+        const ctx = canvas.getContext("2d");
+    
+        resize();
+        
+        //variables
+        let painting = false;
+    
+        function startPosition(e) {
+            painting = true;
+            draw(e);
+        }
+        function finishedPosition() {
+            painting = false;
+            ctx.beginPath();
+        }
+        function draw(e) {
+            if(!painting) return;
+            ctx.lineWidth = 10;
+            ctx.lineCap = "round";
+    
+            ctx.lineTo(e.clientX, e.clientY);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(e.clientX, e.clientY);
+        }
+    
+        //EventListeners
+        canvas.addEventListener("mousedown", startPosition);
+        canvas.addEventListener("mouseup", finishedPosition);
+        canvas.addEventListener("mousemove", draw);
+    });
+    
+    window.addEventListener("resize", resize);
+    
+    function resize() {
+        //Resizing
+        canvas.height = window.innerHeight;
+        canvas.width = window.innerWidth;
+    }
+}
+
+function download(){
+    var download = document.getElementById("download");
+    var image = document.getElementById("canvas").toDataURL("image/png")
+                .replace("image/png", "image/octet-stream");
+    download.setAttribute("href", image);
+
+}
+
+
